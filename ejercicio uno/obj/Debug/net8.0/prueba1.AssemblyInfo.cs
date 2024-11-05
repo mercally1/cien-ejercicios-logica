8b0b528aaf16bfb651a4c385ae7e2753f2373f94
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-11-04 22:54:30 -0600

    change name folder

--- ejercicio uno/obj/Debug/net8.0/prueba1.AssemblyInfo.cs
+++ ejercicio uno/obj/Debug/net8.0/prueba1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f662c675fcc274da8c105ed857a316438689c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
