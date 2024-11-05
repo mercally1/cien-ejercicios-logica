60f662c675fcc274da8c105ed857a316438689c4
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-11-04 22:50:51 -0600

    primer ejercicio

---
+++ prueba1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Bienvenido al programa Fzz Buzz \n");
+
+for(int i= 1; i <= 100; i++){
+    if(i % 3 == 0)
+        Console.WriteLine($"este es el texto FIZZ");
+        if(i % 5 == 0)
+         Console.WriteLine($"este es el texto FIZZBUZZ");
+     else
+        Console.Write($"est el numero {i}\n");
+}
